876f11faecc745525b5e7e77ffdf05e1a26277e6
Sreyleak.EL <Sreyleak.EL@pnc.lan>
2022-03-21 13:14:01 +0700

    #11 log In

--- index.php
+++ index.php
@@ -4,38 +4,42 @@ require_once('models/database.php');
 ?>
 <form action="controllers/login_controller.php" method="get">
     <div class="row container mt-3 w-70 mx-auto">
-        <div class="row bg-light">  
+        
+        <div class="row col-4 mt-3 rounded">
+        <div class="row ">  
                     <h1 class="text text-primary">facebook</h1>
                     <h5 class='text'>Sharing and contacting with people around the world !</h5>
         </div>
-        <div class="row col-4 bg-primary mt-3 rounded">
-            <div class="row ">
-                <div class="col-md-6">
-                        <label for="input_first_name" class="form-label text-light">Frist Name</label>
-                        <input type="text" class="form-control" name="first_name" placeholder="First Name">
+            <div class=" ">
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                            <label for="input_first_name" class="form-label text-primary">Frist Name</label>
+                            <input type="text" class="form-control" name="first_name" placeholder="First Name">
+                    </div>
+                    <div class="col-md-6">
+                            <label for="input_last_name" class="form-label text-primary ">Last Name</label>
+                            <input type="text" class="form-control" name="last_name" placeholder="last Name" >
+                    </div>
                 </div>
-                <div class="col-md-6">
-                        <label for="input_last_name" class="form-label text-light ">Last Name</label>
-                        <input type="text" class="form-control" name="last_name" placeholder="last Name" >
+                <div class="row mt-3 ">
+                    <div class="col-12">
+                            <label for="input_password" class="form-label text-primary ">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Your Password">
+                    </div>
+                </div> 
+                <div class="row mt-5">
+                    <div class="col-12 d-grid ">
+                        
+                            <button type="submit" class="btn btn-primary ">Log In</button>
+                        
+                    </div>
                 </div>
             </div>
-            <div class="row ">
-                <div class="col-12">
-                        <label for="input_password" class="form-label text-light ">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="Your Password">
-                </div>
-            </div>
-            <div class="row ">
-                <div class="col-">
-                    <a href="views/post_view.php" >
-                        <button type="submit" class="btn btn-danger">Log In</button>
-                    </a>
-                </div>
-            </div>
+            
         </div>        
         <div class="row col-8 ">
             <div class="col-12">
-                <img src="../images/login.png" class="img-fluid " alt="">
+                <img src="../images/login.png" class="img-fluid " alt=""> 
             </div>    
                 
         </div>
